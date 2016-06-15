3367a842a195905fc4712a73f80dfab61272b52d
Jacob Spizziri <jacob.spizziri@gmail.com>
2016-06-15 06:28:19 -0500

    fix(labelMarkdown): Fixed labelMarkdown to account for showdown gotchas

--- src/app/services/markdown/labelMarkdown.js
+++ src/app/services/markdown/labelMarkdown.js
@@ -7,13 +7,13 @@
       function labelMarkdown() {
         var quoted = {
           type: 'lang',
-          regex: /\~"(.*?)"\b/g,
+          regex: /~T"(.+?)"/g,
           replace: '<a target="_blank" href="{{ project.web_url }}/issues?label_name=$1"><span class="badge $1">$1</span></a>'
         };
 
         var oneWord = {
           type: 'lang',
-          regex: /\~([^\s]*[^\s0-9][^\s]*)\b/g,
+          regex: /\B~T([^\s]+)\b/g,
           replace: '<a target="_blank" href="{{ project.web_url }}/issues?label_name=$1"><span class="badge $1">$1</span></a>'
         };
 
